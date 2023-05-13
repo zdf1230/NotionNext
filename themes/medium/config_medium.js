9031742bc72dbd0bb514b90bdbacdff7ae98e376
zdf <orientzh1230@gmail.com>
2023-05-13 08:51:18 -0700

    Hide Category and Tag

--- themes/medium/config_medium.js
+++ themes/medium/config_medium.js
@@ -12,8 +12,8 @@ const CONFIG_MEDIUM = {
   POST_DETAIL_TAG: true, // 文章显示标签
 
   // 菜单
-  MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
+  MENU_CATEGORY: false, // 显示分类
+  MENU_TAG: false, // 显示标签
   MENU_ARCHIVE: true, // 显示归档
   MENU_SEARCH: true, // 显示搜索
 
